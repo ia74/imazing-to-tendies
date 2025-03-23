a23dbfac289a906bc981be1c88edd9bd8d87ee43
ia74 <68617740+ia74@users.noreply.github.com>
2025-03-23 00:49:55 -0500

    -fix it

--- imazingToTendies.py
+++ imazingToTendies.py
@@ -1,4 +1,4 @@
-descriptorsPath = "Container/Library/Application Support/PRBPosterExtensionDataStore/61/Extensions/com.apple.WallpaperKit.CollectionsPoster"
+descriptorsPath = "/Container/Library/Application Support/PRBPosterExtensionDataStore/61/Extensions/com.apple.WallpaperKit.CollectionsPoster"
 tmpPath = "./tmp/"
 # import zip
 import zipfile
@@ -17,6 +17,9 @@ if len(sys.argv) > 2:
     outputName = sys.argv[2]
 
 correctedName = zipPath.replace(":", "").replace("\\", "").replace("/", "").replace("?", "").replace("*", "").replace("<", "").replace(">", "").replace("|", "")
+
+temporaryExtractionPath = tmpPath + correctedName
+
 if not zipfile.is_zipfile(zipPath):
     print("Incorrect file format. Is it a zip archive/.imazingapp file?")
     sys.exit(1)
@@ -24,11 +27,11 @@ if not zipfile.is_zipfile(zipPath):
 if not os.path.exists(tmpPath):
     os.makedirs(tmpPath)
 
-if os.path.exists(tmpPath + correctedName):
-    shutil.rmtree(tmpPath + correctedName)
+if os.path.exists(temporaryExtractionPath):
+    shutil.rmtree(temporaryExtractionPath)
 
 with zipfile.ZipFile(zipPath, 'r') as zip_ref:
-    zip_ref.extractall(tmpPath + correctedName)
+    zip_ref.extractall(temporaryExtractionPath)
 
 def dirTree(path, level=0, walk=True):
     tree=[]
@@ -42,18 +45,22 @@ def dirTree(path, level=0, walk=True):
 if not os.path.exists(tmpPath + descriptorsPath):
     print("No PosterBoards found.")
     sys.exit(0)
+
 foundDescriptors = []
 
-realPath = tmpPath + descriptorsPath
-tree = dirTree(tmpPath + descriptorsPath + "/configurations", walk=False)
+realPath = temporaryExtractionPath + descriptorsPath
+tree = dirTree(temporaryExtractionPath + descriptorsPath + "/configurations", walk=False)
+
 for item in tree:
-  print("Found descriptor: " + item +" in " + tmpPath + descriptorsPath + "/configurations/" + item)
-  foundDescriptors.append(tmpPath + descriptorsPath + "/configurations/" + item)
+  print("Found descriptor: " + item +" in " + temporaryExtractionPath + descriptorsPath + "/configurations/" + item)
+  foundDescriptors.append(temporaryExtractionPath + descriptorsPath + "/configurations/" + item)
+
 print("That\'s " + str(len(foundDescriptors)) + " descriptors found.")
 print()
 print("The file with the name " + outputName + ".tendies will be created in the current directory.")
 print("IF A FILE WITH THE SAME NAME ALREADY EXISTS, IT WILL BE OVERWRITTEN.")
 print("Are you sure you want to convert these posters to Tendies (Nugget 5.0+) format? (y/n)")
+
 yesno = input()
 if yesno.lower() != "y":
     print("Exiting.")
